a33228c461398a1846255283743756b2cd4daa4d
Mauro Joel Schutz <mjschutz@outlook.com>
2020-01-30 09:19:38 -0300

    Start files

---
+++ android/src/main/java/com/googleaccount/RNGoogleAccountModule.java
@@ -0,0 +1,52 @@
+package com.googleaccount;
+
+import android.os.AsyncTask;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.io.ByteArrayInputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.AccessToken;
+
+public class RNGoogleAccountModule extends ReactContextBaseJavaModule {
+	private final GoogleCredentials credentials;
+
+	public RNGoogleAccountModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+	}
+
+	@ReactMethod
+	public void fromString(final String jsonData, final Promise promise){
+		AsyncTask.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(jsonData.getBytes()));
+					getAccessToken(promise);
+				} catch (Exception e) {
+					promise.reject(e);
+				}
+			}
+		});
+	}
+	
+	@ReactMethod
+	public void getAccessToken(Promise promise) {
+		try {
+			credentials.refreshIfExpired();
+			AccessToken token = credentials.getAccessToken();
+			promise.resolve(token.getTokenValue());
+		} catch (Exception e) {
+			promise.reject(e);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "RNGoogleAccount";
+	}
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNGoogleAccount } = NativeModules;
+
+export default RNGoogleAccount;
